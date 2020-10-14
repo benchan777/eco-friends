6f58afea6f2f9bde3f9353ece48c45ea486a472d
Ben Chan <benchan777@gmail.com>
2020-10-14 15:25:42 -0700

    add comments

--- script.js
+++ script.js
@@ -42,19 +42,23 @@ function addMarker(location, map) {
     const issue_input = issueInput.value
     const map_label = labels_form[labelIndexForm++ % labels_form.length]
 
+    // Creates new paragraph for form input and remove button
     const form_display = document.createElement('P')
     form_display.id = `input${inputIndex}`
     const delete_button = document.createElement('BUTTON')
     delete_button.id = `delete${inputIndex}`
 
+    // Displays form input data in html page
     form_display.innerHTML = `Marker: ${map_label}<br> Location: ${location_input}<br> Time: ${time_input}<br> Issue: ${issue_input}`
     document.querySelector('#reported-issues').appendChild(form_display)
     delete_button.innerHTML = `Remove`
     document.querySelector('#reported-issues').appendChild(delete_button)
 
+    // Displays form input data in feeds page
     const feed_update_display = document.createElement('P')
     feed_update_display.innerHTML = `${time_input}: A new issue has been reported in ${location_input}!`
     document.querySelector('#feed-updates').appendChild(feed_update_display)
 
+    // Increments form_display and delete_button id by 1
     inputIndex++
 }
