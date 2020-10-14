e918237a1d2cef02c9efff2c840c9f18083f8e2c
Ben Chan <benchan777@gmail.com>
2020-10-13 21:39:53 -0700

    add form submit function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+// display
+const inputDisplay = document.querySelector('#display-inputs')
+
 // inputs
 const locationInput = document.querySelector('#location-input')
 const timeInput = document.querySelector('#time-input')
 const issueInput = document.querySelector('#issue-input')
+const issueSubmit = document.querySelector('#issue-submit')
+
+// event listeners
+issueSubmit.addEventListener('click', formSubmit)
 
 // Initialize and add the map
 function initMap() {
@@ -13,3 +20,13 @@ function initMap() {
     // Marker positioned on Make School
     var marker = new google.maps.Marker({position: make_school, map: map});
   }
+
+function formSubmit() {
+    const location_input = locationInput.value
+    const time_input = timeInput.value
+    const issue_input = issueInput.value
+
+    const form_display = document.createElement('P')
+    form_display.innerHTML = location_input + time_input + issue_input
+    document.querySelector('#reported-issues').appendChild(form_display)
+}
