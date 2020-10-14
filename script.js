dabf9ba404e9b039aeaa51dd92035360ef0f02e5
Ben Chan <benchan777@gmail.com>
2020-10-13 19:44:05 -0700

    change map location

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
+    // Location of Make School
+    var make_school = {lat: 37.783, lng: -122.412};
+    // Map centered on Make School
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+        document.getElementById('map'), {zoom: 14, center: make_school});
+    // Marker positioned on Make School
+    var marker = new google.maps.Marker({position: make_school, map: map});
   }
